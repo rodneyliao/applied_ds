5b01dfe04fdd18ff55b15802985213d6d19021b1
J. Forde <jzf2101@users.noreply.github.com>
2019-02-25 20:22:58 -0800

    added pandas io homework

---
+++ test_io_hw.py
@@ -0,0 +1,10 @@
+import pytest
+import pandas as pd
+import os
+import io_hw
+
+def test_io_hw():
+    df, head_df = io_hw.io_hw('test.csv')
+    assert (os.path.isfile('test.csv')), "file has not been saved"
+    assert sum(1 for line in open('test.csv')) - 1 == len(head_df), 
+    assert len(df.columns) == len(head_df.columns)
