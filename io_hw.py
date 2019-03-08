41a78273959e86af1c313de95db398e25f754051
danhuiZ <danhuizzz@gmail.com>
2019-03-07 20:02:42 -0500

    Saving changes of io_hw.py

--- io_hw.py
+++ io_hw.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import pdb
 
 def io_hw(out_path):
     '''
@@ -10,9 +11,16 @@ def io_hw(out_path):
     
     out_path: a string that is the path to output a csv of the head of your data.
     
+    You will load data, declare new variable for head, save head in out_path
     Your outputs will be:
     
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
+    pdb.set_trace()
+    df = pd.read_csv('https://community.watsonanalytics.com/wp-content/uploads/2015/03/WA_Fn-UseC_-Operations-Dem-Planning_-BikeShare.csv')
     '''
+    df = pd.read_excel('/Users/zhangdanhui/GoogleDrive/MSIM2018-2019/SP2019/EM212_Applied_Data_Science/Homework/Project_New_Seahorse/CellLineResults.xls')
+    head_df = df.head(5)
+    head_df.to_csv(out_path)
     return df, head_df
+
