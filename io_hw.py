39cc0ed2003cacfba18954f5cbabcd82d5c0a26f
RuiShi <Rui.Shi@tufts.edu>
2019-03-07 20:42:28 -0500

    add csv file

--- io_hw.py
+++ io_hw.py
@@ -15,7 +15,7 @@ def io_hw(out_path):
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
-    df = pd.read_csv("~/Downloads/nyc.csv")
+    df = pd.read_csv("./sample.csv")
     head_df = df.head()
     head_df.to_csv(out_path)
     return df, head_df
