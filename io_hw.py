402d2f127f7e0b08aacf6611df6977adf860acec
Chao Wu <chao@Chaos-MBP.fios-router.home>
2019-03-10 16:02:46 -0400

    For homework#1

--- io_hw.py
+++ io_hw.py
@@ -2,17 +2,11 @@ import pandas as pd
 import seaborn as sns
 
 def io_hw(out_path):
-    '''
-    This is your homework assignment.
-    You will be wrting a function here.
     
-    Your input will be:
+    df = pd.read_csv("NYC-RI.csv")  #The full pandas dataframe of the dataset
     
-    out_path: a string that is the path to output a csv of the head of your data.
+    head_df = df.head() #A new dataframe that is a copy of the first 5 lines of the dataframe, df
     
-    Your outputs will be:
+    head_df.to_csv(out_path)
     
-    df: The full pandas dataframe of your dataset.
-    head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
-    '''
     return df, head_df
