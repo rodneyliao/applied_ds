e75ec0c518f6f1d2a2e9861e76b4ea5303cd1d94
Chao Wu <chao@Chaos-MBP.fios-router.home>
2019-03-10 16:58:21 -0400

    Test of homework#1

--- io_hw.py
+++ io_hw.py
@@ -2,8 +2,9 @@ import pandas as pd
 import seaborn as sns
 
 def io_hw(out_path):
-    
-    df = pd.read_csv("NYC-RI.csv")  #The full pandas dataframe of the dataset
+
+    df = pd.read_csv('./NYC-RI.csv') 
+    #The full pandas dataframe of the dataset
     
     head_df = df.head() #A new dataframe that is a copy of the first 5 lines of the dataframe, df
     
