7b5ffafb0cab780890f10412ea92ec3c62a490fa
Chao Wu <>
2019-03-10 22:51:01 -0400

    upload example file

--- io_hw.py
+++ io_hw.py
@@ -4,7 +4,7 @@ import seaborn as sns
 def io_hw(out_path):
 
     df = pd.read_csv('./data.csv') 
-    #The full pandas dataframe of the dataset
+    #The full pandas dataframe of dataset
 
     head_df = df.head() #A new dataframe that is a copy of the first 5 lines of the dataframe, df
     
