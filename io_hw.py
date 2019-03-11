684c3d28814556f28a0ebca452abf3fbbcf80819
Shea T. Nelson <Shea@Sheas-MacBook-Pro-5.local>
2019-03-11 19:33:27 -0400

    This is a script that returns the head of my em-212 dataset for project

--- io_hw.py
+++ io_hw.py
@@ -11,7 +11,6 @@ def io_hw(out_path):
     out_path: a string that is the path to output a csv of the head of your data.
     
     Your outputs will be:
-    
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
@@ -19,6 +18,7 @@ def io_hw(out_path):
     
     
     head_df = df.head()
+    
     head_df.to_csv('./test.csv')
 
     return df, head_df
