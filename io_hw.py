300eff83ec828a92c456edf1af5db69be25cb11a
Konstantinos Tzortzakis <konstantinostzortzakis@Konstantinoss-MacBook-Pro.local>
2019-03-12 14:57:56 -0400

    pull please

--- io_hw.py
+++ io_hw.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import pdb
 
 def io_hw(out_path):
     '''
@@ -15,4 +16,9 @@ def io_hw(out_path):
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
+    df = pd.read_csv('./WA_Fn-UseC_-Telco-Customer-Churn.csv')
+    head_df = df.head(5)
+    head_df.to_csv(out_path)
     return df, head_df
+    
+  
