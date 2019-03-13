070875c0a7c7753912cae77d3a4b5398c161e91c
Nick <fanyiwen9596@hotmail.com>
2019-03-13 17:25:09 -0400

    Add files via upload

--- io_hw.py
+++ io_hw.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import seaborn as sns
 
@@ -14,5 +15,14 @@ def io_hw(out_path):
     
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
-    '''
+        '''
+    df = pd.read_csv('sample.csv')
+    head_df = df.head()
+    head_df.to_csv(out_path,index = False)
+    print("Finished")
     return df, head_df
+
+'''
+
+io_hw('/Users/Fanyiwen/applied_ds/data_result/test.csv')
+'''
