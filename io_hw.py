4841e88941f4c1adda630a021f2bab416978e926
Ted Yuan <tedyuan@Teds-MacBook-Pro-2.local>
2019-03-13 14:03:47 -0400

    Updated code with local and url dataset

--- io_hw.py
+++ io_hw.py
@@ -20,7 +20,7 @@ def io_hw(out_path):
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
    
-    ##df = pd.read_csv('https://www.kaggle.com/lava18/google-play-store-apps/downloads/googleplaystore.csv')
+  
     ##df = pd.read_csv('./Data/googleplaystore.csv')
     df = pd.read_csv('https://tufts.box.com/shared/static/8utsdmfx97s0o1j63l1ogwr3822coo6l.csv')
     head_df = df.head(5)
