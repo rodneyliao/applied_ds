aeecec0c236d375f34b2fc622445d69b4f4867f7
vincentahwang <48077850+vincentahwang@users.noreply.github.com>
2019-03-12 21:22:45 -0400

    Update io_hw.py

--- io_hw.py
+++ io_hw.py
@@ -15,4 +15,9 @@ def io_hw(out_path):
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
+    filepath = 'https://www.dropbox.com/s/egrqg5hcja6i5r5/ACSDATAFinal.csv?dl=0'
+    df = pd.read_csv(filepath)
+    head_df = df.head()
+    output = head_df.to_csv(outpath)
+    print(type(head_df))
     return df, head_df
