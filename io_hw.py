b84d8ab64813594286d12f4c4df36725eb0d228e
Lulu Li <tiffanyli@Lulus-MacBook-Pro.local>
2019-03-13 21:54:57 -0400

    changed io_hw.py to import dataset

--- io_hw.py
+++ io_hw.py
@@ -5,14 +5,21 @@ def io_hw(out_path):
     '''
     This is your homework assignment.
     You will be wrting a function here.
-    
+
     Your input will be:
-    
+
     out_path: a string that is the path to output a csv of the head of your data.
-    
+
     Your outputs will be:
-    
+
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
+
+    url = "https://data.kingcounty.gov/resource/murn-chih.json"
+    df = pd.read_json(url)
+    
+    head_df = df.head(5)
+    head_df.to_csv(out_path)
+    
     return df, head_df
