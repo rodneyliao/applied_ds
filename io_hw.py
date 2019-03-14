48de86f9a097814efe2be4ffa0726a3ee28ab816
rodneyliao <48111210+rodneyliao@users.noreply.github.com>
2019-03-13 23:22:47 -0400

    Add files via upload

--- io_hw.py
+++ io_hw.py
@@ -15,4 +15,7 @@ def io_hw(out_path):
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
+    df = pd.read_csv("https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv")
+    head_df = df.head()
+    head_df.to_csv(out_path, index = False)
     return df, head_df
