3dd342b08fc5fbd05b693a1a538d71bfe724d4fe
Ted Yuan <tedyuan@Teds-MacBook-Pro-2.local>
2019-03-30 14:18:53 -0400

    Ted static hw

--- eda_hw.py
+++ eda_hw.py
@@ -14,7 +14,14 @@ def average(series):
     See numpy documenation for implementation details:
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
     """
-    pass
+    #sl = len(series)
+    #ss = sum(series)
+    #sa = ss / sl
+    sa = sum(series) / len(series) 
+    return sa
+
+#x = average([1,2,3,4,5,6,7,8])
+#print(average([1,2,3,4,5,6,7,8]))
 
 def standard_deviation(series):
     """
@@ -26,7 +33,20 @@ def standard_deviation(series):
     See numpy documenation for implementation details:
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.std.html
     """
-    pass
+    sa = average(series)
+    sl = len(series) 
+    ss = 0
+    i=1
+    for i in range(0,sl):
+        x = series[i] - sa 
+        y = x ** 2
+        ss = ss + y
+        #print (x, y, ss, i)
+    #print (ss)
+    sd = math.sqrt(ss/(sl-1))
+    return sd
+    
+#print(standard_deviation([1,2,3,4]))
 
 def median(series):
     """
@@ -38,4 +58,12 @@ def median(series):
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.median.html
     https://pandas.pydata.org/pandas-docs/version/0.23.0/generated/pandas.Series.median.html
     """
-    pass
+    sl = len(series)
+    if sl < 1:
+            return None
+    if sl % 2 == 1:
+            return sorted(series)[sl//2]
+    else:
+            return sum(sorted(series)[sl//2-1:sl//2+1])/2.0
+    
+    
