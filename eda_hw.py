302190e433afb5af53bc802f2dc5a447b691b779
Nadia Ferrer <NadiaFerrer@Nadias-MacBook-Air.local>
2019-03-31 15:05:00 -0400

    Nadia's EDA HW

--- eda_hw.py
+++ eda_hw.py
@@ -4,38 +4,41 @@ import math
 import pdb
 
 def average(series):
-    """
-    implements the average of a pandas series from scratch
-    suggested functions:
-    len(list)
-    sum(list)
-    you should get the same result as calling .mean() on your series
-    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.mean.html
-    See numpy documenation for implementation details:
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
-    """
+   
+    # implements average of pandas series from scratch    
+    avg = sum(series)/len(series)
+    return avg
     pass
 
 def standard_deviation(series):
-    """
-    implements the sample standard deviation of a series from scratch
-    you may need a for loop and your average function
-    also the function math.sqrt
-    you should get the same result as calling .std() on your data
-    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.std.html
-    See numpy documenation for implementation details:
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.std.html
-    """
+   
+    # implements sample standard deviation of a series from scratch
+    
+    # go through loop to find variance
+    n = len(series)
+    sqDiff = 0
+    variance = 0
+    for idx in series:
+        sqDiff = ((idx - average(series))**2)
+        variance += sqDiff
+    std = math.sqrt(variance/n)
+    
+    return std
     pass
 
 def median(series):
-    """
-    finds the median of the series from scratch
-    you may need to sort your values and use
-    modular division
-    this number should be the same as calling .median() on your data
-    See numpy documenation for implementation details:
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.median.html
-    https://pandas.pydata.org/pandas-docs/version/0.23.0/generated/pandas.Series.median.html
-    """
+   
+    # implements median of series from scratch
+    
+    #sorts series
+    s = sorted(series)
+    n = len(series)
+    m = n+1
+    
+    # determines whether even or odd and determines median
+    if (n % 2) == 0:
+        return (s[int(n/2)]+ s[int(n/2)+1])/2
+    else:
+        return s[int(m/2)]
+    
     pass
