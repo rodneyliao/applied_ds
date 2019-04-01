4fa1c1620161aed3ab4e21c99460dbb3513b2d1b
Ottillia Ni <ottilliani@Ottillias-MacBook-Pro.local>
2019-03-31 21:41:25 -0400

    Ottillia HW2

--- eda_hw.py
+++ eda_hw.py
@@ -3,47 +3,26 @@ import numpy as np
 import math
 import pdb
 
+
 def average(series):
-    return sum(list)/len(list)
+    return sum(series)/len(series)
 
-list = [1, 19, 20, 18, 3, 6, 8, 10, 2, 5, 7]
-avg = average list
-
-print ("Average of the list =", round(avg,2))
-    
-    """
-    implements the average of a pandas series from scratch
-    suggested functions:
-    len(list)
-    sum(list)
-    you should get the same result as calling .mean() on your series
-    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.mean.html
-    See numpy documenation for implementation details:
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
-    """
-    pass
 
 def standard_deviation(series):
-    
-    """
-    implements the sample standard deviation of a series from scratch
-    you may need a for loop and your average function
-    also the function math.sqrt
-    you should get the same result as calling .std() on your data
-    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.std.html
-    See numpy documenation for implementation details:
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.std.html
-    """
-    #pass
+    y=average(series)
+    n = 0 
+    for i in series:
+        n = n + (i-y)**2
+    x = len(series)
+    return math.sqrt(n/(x-1))
 
-#def median(series):
-    """
-    finds the median of the series from scratch
-    you may need to sort your values and use
-    modular division
-    this number should be the same as calling .median() on your data
-    See numpy documenation for implementation details:
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.median.html
-    https://pandas.pydata.org/pandas-docs/version/0.23.0/generated/pandas.Series.median.html
-    """
-    #pass
+
+def median(series):
+    t = len(series)
+    if t < 1:
+            return None
+    if t % 2 == 1:
+            return sorted(series)[t//2]
+    else:
+            return sum(sorted(series)[t//2-1:t//2+1])/2
+   
