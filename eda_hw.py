87cd3397442e4dda8ad22823f2e335ec78b518a9
Konstantinos Tzortzakis <konstantinostzortzakis@Konstantinoss-MacBook-Pro.local>
2019-04-17 23:18:00 -0400

    push_again_eda

--- eda_hw.py
+++ eda_hw.py
@@ -2,21 +2,35 @@ import pandas as pd
 import numpy as np
 import math
 import pdb
+import random
+from math import sqrt
+
+
 
 def average(series):
-    """
-    implements the average of a pandas series from scratch
-    suggested functions:
-    len(list)
-    sum(list)
-    you should get the same result as calling .mean() on your series
-    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.mean.html
-    See numpy documenation for implementation details:
-    https://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
-    """
-    pass
+
+    y = len(series)
+    x = sum(series)
+
+    av = x/y
+    return(av)
+    
 
 def standard_deviation(series):
+
+    r = 0
+    aver = average(series)
+
+    for x in range (len(series)):
+        k = abs( series[x] - aver)**2
+        r = r + k
+    
+    
+    ss = r/(len(series)-1)
+    if ss>0 :
+        return (sqrt(ss))
+    else:
+        return(n/a)
     """
     implements the sample standard deviation of a series from scratch
     you may need a for loop and your average function
@@ -26,9 +40,21 @@ def standard_deviation(series):
     See numpy documenation for implementation details:
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.std.html
     """
-    pass
+
 
 def median(series):
+
+    l=sorted(series)
+    t= len(l)
+
+    if t<1:
+        return None
+    if t % 2 == 0:
+        return (l[int(t/2)] + l[int(t/2)-1]) / 2
+    else:
+        return l[int((t-1)/2)]
+    
+
     """
     finds the median of the series from scratch
     you may need to sort your values and use
@@ -38,4 +64,3 @@ def median(series):
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.median.html
     https://pandas.pydata.org/pandas-docs/version/0.23.0/generated/pandas.Series.median.html
     """
-    pass
